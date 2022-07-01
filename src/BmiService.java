99bd8df558a36c8fb2373e021d7fec5ac4c780dc
Ksushka1993 <buslovdn@gmail.com>
2022-07-01 15:22:43 +0700

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+    public double calculate ( double height, double weight) {
+
+        double bmi = weight / ((height/100) * (height/100));
+        return bmi;}
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+
+            double bmi = service.calculate(187, 87);
+        System.out.println(bmi);
+
+    }
+}
+
