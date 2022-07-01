0c1d3d973a3a4657499bf348692197bb4899ec13
Ksushka1993 <buslovdn@gmail.com>
2022-07-01 17:17:55 +0700

    change added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         BmiService service = new BmiService();
-
-            double bmi = service.calculate(187, 87);
+            double height = 187; //cm
+            double weight = 87; //kg
+            double bmi = service.calculate(height, weight);
         System.out.println(bmi);
 
     }
